78f4441ff5de66c9a9542514b53a981ae125d639
Jairus Bali <jairus.bali@gmail.com>
2019-05-21 03:57:05 -0400

    made section place holders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { createGlobalStyle } from "styled-components";
 
 import AppBar from "./AppBar/AppBar";
 import Header from "./components/Header/Header";
+import Works from "./components/Works/Works";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
       <AppBar />
       <Header />
+      <Works />
+      <Contact />
+      <Footer />
     </React.Fragment>
   );
 }
